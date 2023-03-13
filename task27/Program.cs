9a005d2b40b00a00dc05ae447e4f278377644050
Anrmnk <anrmnk@gmail.com>
2023-03-13 15:48:55 +0300

    27 task ok

---
+++ task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int write(string messageToUser)
+{
+    Console.Write (messageToUser);
+    return Convert.ToInt32 (Console.ReadLine());
+}
+
+int sum(int a)
+{
+    int summ = 0;
+    while (a > 10)
+    {
+        summ = a % 10 + summ;
+        a = a / 10;
+    }
+    summ = summ + a;
+    return summ;
+}
+
+int num1 = write ("Введите число ");
+Console.WriteLine ($"Сумма чисел равна = {sum (num1)}");
