62aeb8af6244adbc2bb1907884ddd6c20d8419ad
Anrmnk <anrmnk@gmail.com>
2023-03-13 16:20:18 +0300

    29 task ok

---
+++ task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+int[] array = newint();
+
+int[] newint()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($" Введите {i} число ");
+        int num = int.Parse(Console.ReadLine());
+        array[i] = num;
+    }
+    return array;
+}
+Console.WriteLine(string.Join(" ", array));
